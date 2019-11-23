b6ead845e2b05eb9579ca9d719086a699247285a
tausvels <engr.tausifkhan@gmail.com>
2019-11-22 21:31:34 -0800

    All the nesscessary files have been uploaded

---
+++ index.js
@@ -0,0 +1,3 @@
+import sum from './sketchboard';
+
+module.exports = sum;
---
+++ sketchboard.js
@@ -0,0 +1,20 @@
+//using jquery 
+
+//Creating a simple arithmatic function
+const sum = (a,b) => {
+  var result = a+b;
+  console.log(result)
+  return result
+}
+
+$(document).ready(()=>{
+  // var input1 = window.prompt('Enter number 1:' + '')
+  // var input2 = window.prompt('Enter number 2:' + '')
+
+  // input1 = parseInt(input1);
+  // input2 = parseInt(input2)
+
+  // sum(input1, input2)
+})
+
+module.exports = sum;
